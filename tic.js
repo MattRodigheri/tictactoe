a1a4dfb13a6de5d1d2217130e8a97bd240cc27d8
MattRodigheri <mrodigheri@gmail.com>
2018-10-24 14:12:54 -0700

    dependencies

---
+++ tic.js
@@ -0,0 +1,28 @@
+const readline = require('readline');
+const mongoose = require('mongoose');
+const assert = require('assert');
+mongoose.Promise = global.Promise;
+
+const db = mongoose.connect('mongodb://localhost:27017/tictactoe');
+
+const gameSchema = mongoose.Schema({
+  1: { type: String },
+  2: { type: String },
+  3: { type: String },
+  4: { type: String },
+  5: { type: String },
+  6: { type: String },
+  7: { type: String },
+  8: { type: String },
+  9: { type: String }
+});
+
+const Game = mongoose.model('Game', gameSchema);
+
+const playGame = (move) => {
+  Game.create(move, (err) => {
+    assert.equal(null, err);
+    console.log(move);
+    db.disconnect();
+  });
+};
